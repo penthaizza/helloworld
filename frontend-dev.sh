3dba84728c603f54566c86077080be7faa9a190e
penthaizza <penthaizza@gmail.com>
2019-07-12 15:03:18 +0700

    [TEST] use function in shellscript

--- frontend-dev.sh
+++ frontend-dev.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
+installaws(){
+    echo "Install awscli"
+    pip install awscli
+    pip install awscli --upgrade
+    echo "Check Version AWS CLI"
+    aws --version
+}
 
-echo "Install awscli"
-pip install awscli
-pip install awscli --upgrade
+installdocker(){
+    echo "Install Docker"
+    set -x
+    VER="18.09.7"
+    curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
+    tar -xz -C /tmp -f /tmp/docker-$VER.tgz
+    mv /tmp/docker/* /usr/bin
+}
 
-echo "Check Version AWS CLI"
-aws --version
+setawsenv(){
+    echo "Set configure AWS"
+    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    aws configure set default.region $AWS_DEFAULT_REGION
+}
 
-echo "Install Docker"
-set -x
-VER="18.09.7"
-curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
-tar -xz -C /tmp -f /tmp/docker-$VER.tgz
-mv /tmp/docker/* /usr/bin
+pushtoecr(){
+    echo "Build & Push to ECR"
+    $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+    docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$FRONTEND_MICRO:$(git rev-parse --short HEAD) .
+    docker tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$FRONTEND_MICRO:$(git rev-parse --short HEAD) $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$FRONTEND_MICRO:latest
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$FRONTEND_MICRO
+}
 
-echo "Set configure AWS"
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set default.region $AWS_DEFAULT_REGION
-
-echo "Build & Push to ECR"
-$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
-docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_RESOURCE_NAME_PREFIX:$(git rev-parse --short HEAD) .
-docker tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_RESOURCE_NAME_PREFIX:$(git rev-parse --short HEAD) $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_RESOURCE_NAME_PREFIX:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_RESOURCE_NAME_PREFIX
+installaws
+installdocker
+setawsenv
+pushtoecr
