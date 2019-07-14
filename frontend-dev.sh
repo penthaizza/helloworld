e0d372a333cb30c8a94ec624bd0258d78b70946e
penthaizza <penthaizza@gmail.com>
2019-07-14 15:57:11 +0700

    [EDIT] remove sudo

--- frontend-dev.sh
+++ frontend-dev.sh
@@ -29,8 +29,8 @@ setawsenv(){
 
 installecs(){
     echo "Install ECS_CLI"
-    sudo curl -o /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
-    sudo chmod +x /usr/local/bin/ecs-cli
+    curl -o /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
+    chmod +x /usr/local/bin/ecs-cli
     ecs-cli --version
 }
 
